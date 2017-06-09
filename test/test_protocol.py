15fdf424d5d988c52fa891964d99787f378d731f
jonse <jonseb1998@gmail.com>
2017-06-09 22:37:20 +0200

    Added a utility class to the test module, which creates a pair of connected sockets

--- test/test_protocol.py
+++ test/test_protocol.py
@@ -4,6 +4,7 @@ import socket
 import random
 import threading
 import json
+import time
 
 
 def open_port():
@@ -14,8 +15,8 @@ def open_port():
     """
     # Choosing the port random in the upper port range
     port = random.randint(30000, 60000)
-    result = 1
-    while result != 0:
+    result = 0
+    while result == 0:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         address = ('127.0.0.1', port)
         # attempting to connect to the server, in case not possible, there is no socket on the port -> port is free
@@ -23,6 +24,7 @@ def open_port():
         port = random.randint(30000, 60000)
     # Returning the found free port
     return port
+
 
 class TestForm(unittest.TestCase):
 
@@ -109,13 +111,69 @@ class TestForm(unittest.TestCase):
         self.assertTrue(form.empty)
 
 
-
 class SockGrab(threading.Thread):
-
-    def __init__(self):
+    """
+    This is a utility class, which will open a server socket at the given port and then simply wait for the first
+    incoming connection and then assign the bound socket, which is the result of this first established connection to
+    the internal 'connection' attribute, where it can be grabbed for further use.
+    """
+    def __init__(self, port):
         threading.Thread.__init__(self)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect()
+        self.address = ('127.0.0.1', port)
+        self.connector = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection = None
+
+    def run(self):
+        """
+        The main method of the Thread, which will be called after the Thread was started. This will simply define the
+        internal socket object as a server and wait for a connection, which it then assigns to the connection attribute
+        Returns:
+        void
+        """
+        self.sock.bind(self.address)
+        self.sock.listen(2)
+        self.connection, address = self.sock.accept()
+
+    def sockets(self):
+        """
+        This method returns the pair of connected sockets.
+        Returns:
+        The
+        """
+        # Connecting the connector
+        self.connect()
+        # Waiting for the corresponding socket was accepted in the server socket
+        self.get_connection()
+        return self.connection, self.connector
+
+    def get_connection(self):
+        """
+        This method is used to get the connected server socket from the Thread. The method will be blocking until the
+        connection attribute is assigned the connected socket from the server accepting or the timeout of waiting one
+        second has been exceeded
+        Raises:
+            TimeoutError: In case, that even one second after the method was called, the socket was sill not assigned
+                to the connection attribute of this object
+        Returns:
+        The socket object, that was given by the server for the established connection
+        """
+        start_time = time.time()
+        while self.connection is None:
+            delta = time.time() - start_time
+            if delta > 1:
+                raise TimeoutError("Connection problems in testing")
+        return self.connection
+
+    def connect(self):
+        """
+        This method will simply connect the internal connector socket to the server. Thus it has to be called only
+        after the Thread has been started
+        Returns:
+        void
+        """
+        self.connector.connect(self.address)
+
 
 class TestFormTransmission(unittest.TestCase):
 
@@ -123,8 +181,10 @@ class TestFormTransmission(unittest.TestCase):
     std_port = 56777
 
     def test_init(self):
-
-        transmitter = protocol.FormTransmitterThread()
+        port = open_port()
+        s = SockGrab(port)
+        s.start()
+        print(s.sockets())
 
     def socket_pair(self):
         pass
